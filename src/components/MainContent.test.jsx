c0f9e7ff6d43b37c90c2296b847f0780699c0342
vckotwad <94042236+vckotwad@users.noreply.github.com>
2024-12-03 16:16:41 +0530

    Update MainContent.test.jsx

--- src/components/MainContent.test.jsx
+++ src/components/MainContent.test.jsx
@@ -4,7 +4,7 @@ import userEvent from '@testing-library/user-event';
 
 import MainContent from './MainContent';
 
-// My tests!
+// My tests! for testing
 // Add yet another comment
 
 describe('MainContent', () => {
